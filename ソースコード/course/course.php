2e18b69f2093a03226ec4dcd97a6eb4e693732eb
Aso2001210 <83047935+Aso2001210@users.noreply.github.com>
2022-06-21 12:26:41 +0900

    Add files via upload

---
+++ ソースコード/course/course.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>おすすめコース</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<h1><a href="cosu.php" class="title">三度の飯より明太子</a></h1>
+<a href=""><button type="submit" class="btn" style="position: absolute; width: 130px; height: 40px; right: 900px; top: 70px">観光</button></a>
+<a href=""><button type="submit" class="btn2" style="position: absolute; width: 130px; height: 40px; right: 700px; top: 70px">グルメ・郷土料理</button></a>
+<a href=""><button type="submit" class="btn2" style="position: absolute;width: 130px; height: 40px; right: 500px; top: 70px">お土産</button></a><br>
+<a href=""><button type="submit" class="btn2" style="position: absolute;width: 130px; height: 40px; right: 300px; top: 70px">おすすめコース</button></a><br>
+<a href=""><button type="submit" class="btn2" style="position: absolute;width: 130px; height: 40px; right: 100px; top: 70px">予約</button></a><br>
+<div class="flex">
+    <figure class="image"><img src="img/Hakataeki.jpg"></figure>
+    <div class="right">
+        <br>
+        <p class="title1">博多駅</p>
+        <br><br>
+        <p class="text">非常に大きな駅で買い物や食事が出来<br>
+            有名店が多々あります<br>
+            外では季節ごとに様々なイベントが行われています<br>
+            交通面でも新幹線、電車、地下鉄、バス、タクシーと<br>
+            多くの交通機関を利用することが出来ます
+        </p>
+    </div>
+</div>
+<div class="v_line_left">
+    <br>
+    博多駅、〒814-0012　福岡県福岡市博多区博多駅<br>
+    <br>
+    徒歩　2分<br>
+    140m<br>
+    博多バスターミナル1F (バス)<br>
+    113快　片江営業所行　快速[観光道路・油山団地]<br>
+        <br>
+        キャナルシティ博多 (バス)<br>
+        <br>
+        徒歩 3分<br>
+        200m<br>
+        キャナルシティ博多、〒812-0018 福岡県福岡市博多区住吉1丁目2
+    <br>
+</div>
+<div class="flex">
+    <figure class="image"><img src="img/Kyanaru.jpg"></figure>
+    <div class="right">
+        <br>
+        <p class="title1">キャナルシティ博多</p>
+        <br><br>
+        <p class="text">非常に大きな駅で買い物や食事が出来<br>
+            有名店が多々あります<br>
+            外では季節ごとに様々なイベントが行われています<br>
+            交通面でも新幹線、電車、地下鉄、バス、タクシーと<br>
+            多くの交通機関を利用することが出来ます
+        </p>
+    </div>
+</div>
+</body>
+</html>
