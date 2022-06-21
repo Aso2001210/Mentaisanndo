780ba75709a038e5b97f67ef3171bce573d86a22
Aso2001210 <83047935+Aso2001210@users.noreply.github.com>
2022-06-21 12:42:09 +0900

    Add files via upload

---
+++ ソースコード/yoyaku/yoyaku.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>予約</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<h1><a href="cosu.php" class="title">三度の飯より明太子</a></h1>
+<a href=""><button type="submit" class="btn" style="position: absolute; width: 130px; height: 40px; right: 900px; top: 70px">観光</button></a>
+<a href=""><button type="submit" class="btn2" style="position: absolute; width: 130px; height: 40px; right: 700px; top: 70px">グルメ・郷土料理</button></a>
+<a href=""><button type="submit" class="btn2" style="position: absolute;width: 130px; height: 40px; right: 500px; top: 70px">お土産</button></a><br>
+<a href=""><button type="submit" class="btn2" style="position: absolute;width: 130px; height: 40px; right: 300px; top: 70px">おすすめコース</button></a><br>
+<a href=""><button type="submit" class="btn2" style="position: absolute;width: 130px; height: 40px; right: 100px; top: 70px">予約</button></a><br>
+<p class="title1">予約<br>
+    <br>
+    航空券<br>
+    ANA:<a href="https://www.ana.co.jp" target="_blank">https://www.ana.co.jp</a><br>
+    JAL:<a href="https://www.jal.co.jp/jp/ja/" target="_blank">https://www.jal.co.jp/jp/ja/</a><br>
+    FDA:<a href="https://www.fujidream.co.jp" target="_blank">https://www.fujidream.co.jp</a><br>
+    <br>
+    新幹線・JR特急列車<br>
+    えきねっと:<a href="https://www.eki-net.com/personal/top/index" target="_blank">https://www.eki-net.com/personal/top/index</a><br>
+    <br>
+    高速バス<br>
+    バス比較ナビ:<a href="https://www.bushikaku.net/" target="_blank">https://www.bushikaku.net/</a><br>
+    <br>
+    ツアー<br>
+    ANA トラベラーズダイナミックパッケージ<br>
+    <a href="https://www.ana.co.jp/ja/jp/domtour/dp/" target="_blank">https://www.ana.co.jp/ja/jp/domtour/dp/</a>
+</p>
+</body>
+</html>
+
