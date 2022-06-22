395be995d80eadf9f19ac9d10d3b2abd3a75771a
Aso2001210 <83047935+Aso2001210@users.noreply.github.com>
2022-06-22 09:54:13 +0900

    Add files via upload

---
+++ ソースコード/toppage/toppage.php
@@ -0,0 +1,70 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>トップページ</title>
+    <link rel="stylesheet" href="./css/top.css">
+</head>
+<body>
+
+<h1 ><a href="toppage.php" class="title">三度の飯より明太子</a></h1>
+<a href=""><button type="submit" class="btn" style="position: absolute; width: 130px; height: 40px; right: 900px; top: 70px">観光</button></a>
+<a href=""><button type="submit" class="btn2" style="position: absolute; width: 130px; height: 40px; right: 700px; top: 70px">グルメ・郷土料理</button></a>
+<a href=""><button type="submit" class="btn2" style="position: absolute;width: 130px; height: 40px; right: 500px; top: 70px">お土産</button></a><br>
+<a href=" course.php"><button type="submit" class="btn2" style="position: absolute;width: 130px; height: 40px; right: 300px; top: 70px">おすすめコース</button></a><br>
+<a href="Yoyaku.php"><button type="submit" class="btn2" style="position: absolute;width: 130px; height: 40px; right: 100px; top: 70px">予約</button></a><br>
+<img src="img/1.png" style="position: absolute;width: 1500px; height: 600px; top: 150px"  >
+<font size=4>
+    <h1 style="position:absolute; right: 550px;  top: 750px">遊び・体験</h1>
+
+    <img src="img/2.png"style="position: absolute;width: 650px; height: 400px; top: 850px"  >
+    <h1 style="position:absolute; right: 500px;  top: 850px">NOBOLT</h1>
+    <font size=5>
+        <p style="position:absolute; right: 100px;  top: 950px">屋内アスレチック施設<br>16種類のアトラクションがあり
+            <br>友達と楽しみながら汗をかける素晴らしい施設です</p>
+
+        <img src="img/3.png"style="position: absolute;width: 300px; height: 300px; top: 1300px"  >
+        <img src="img/4.png"style="position: absolute;width: 300px; height: 300px; right: 700px; top: 1300px"  >
+        <a href="https://www.taito.co.jp/NOBOLT"><p style="position:absolute; right: 250px;  top: 1200px">ホームページ<br>https://www.taito.co.jp/NOBOLT</p></a>
+        <a href="https://webket.jp/pc/ticket/index"><p style="position:absolute; right: 250px;  top: 1400px">予約ページ<br>https://webket.jp/pc/ticket/index</p></a>
+
+        <img src="img/5.png"style="position: absolute;width: 400px; height: 250px; top: 1750px"  >
+        <img src="img/6.png"style="position: absolute;width: 400px; height: 250px; right: 700px; top: 2050px"  >
+        <font size=4>
+            <h1 style="position:absolute; right: 200px;  top: 1700px">BOSS E・ZO FUKUOKA </h1>
+            <font size=5>
+                <p style="position:absolute; right: 50px;  top: 1950px">絶叫系のアトラクションからバーチャルコンテンツの<br>
+                    体験が出来ます。汗をかいた後はPayPayDomeで<br>
+                    野球観戦をするのも良いでしょう
+                </p>
+                <a href="https://e-zofukuoka.com/"><p style="position:absolute; right: 300px;  top: 2200px">ホームページ<br>https://e-zofukuoka.com/</p></a>
+
+                <img src="img/7.png"style="position: absolute;width:600px; height: 400px; right: 700px; top: 2450px"  >
+                <font size=4>
+                    <h1 style="position:absolute; right: 100px;  top: 2400px">中道海浜公園セグウェイツアー</h1>
+                    <font size=5>
+                        <p style="position:absolute; right: 250px;  top: 2600px">公園内をセグウェイを乗って回る<br>
+                            なかなか体験できないツアーです<br>
+                            心地よく走り抜けよう
+                        </p>
+                        <a href="https://segwaysmile.jp/tour/uminaka/"><p style="position:absolute; right: 150px;  top: 2800px">ホームページ<br>https://segwaysmile.jp/tour/uminaka/</p></a>
+
+                        <font size=4>
+                            <h1 style="position:absolute; right: 600px;  top: 3000px">おすすめ</h1>
+                            <img src="img/8.png"style="position: absolute;width:700px; height: 300px; right: 350px; top: 3100px"  >
+                            <h1 style="position:absolute; right: 600px;  top: 3400px">宮地嶽神社</h1>
+                            <font size=5>
+                                <center><p style="position:absolute; right: 400px;  top: 3500px">光の道と呼ばれるまっすぐ伸びる参道の延長線上に<br>
+                                        夕日が沈む絶景が見られる</center>
+
+                                </p>
+
+
+                            </font>
+
+</body>
+
+</html>
