22459fbef44e29feebc29ee594ce480c3677c462
Aso2001210 <83047935+Aso2001210@users.noreply.github.com>
2022-06-22 09:53:30 +0900

    Add files via upload

---
+++ ソースコード/shinrei/shinrei.php
@@ -0,0 +1,36 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>心霊スポット</title>
+    <link rel="stylesheet" href="./css/spot.css">
+</head>
+<body>
+
+<h1 ><a href="toppage.php" class="title">三度の飯より明太子</a></h1>
+<font size=3>
+    <a class="btn" href="" style="position: absolute; width: 130px; height: 40px; right: 900px; top: 70px">観光</a>
+    <a class="btn" href="" style="position: absolute; width: 130px; height: 40px; right: 750px; top: 70px">グルメ・郷土料理</a>
+    <a class="btn" href="" style="position: absolute;width: 130px; height: 40px; right: 500px; top: 70px">お土産</a>
+    <a class="btn" href=" course.php" style="position: absolute;width: 130px; height: 40px; right: 300px; top: 70px"> おすすめコース</a>
+    <a class="btn" href="Yoyaku.php" style="position: absolute;width: 130px; height: 40px; right: 50px; top: 70px">予約</a>
+</font>
+
+<img src="img/shinrei.png"style="position: absolute;width: 650px; height: 400px; right: 400px; top: 200px"  >
+
+<font size=5>
+    <h1 class="btn" style="position:absolute; right: 630px;  top: 600px">牛頸ダム</h1>
+</font>
+
+<font size=4>
+    <center><p class="btn" style="position:absolute; right: 500px;  top: 800px">事件や自谿ｺも多く非常に有名な心霊スポット<br>
+            数多くの心霊現象が報告されており、<br>
+            赤い服を着ていくことはやめておくことをおすすめする<br>
+            実際に立ち入り禁止の場所もあるので注意して行こう</center>
+</font>
+
+</body>
+
+</html>
